a3b3bcfedf476272239bac3e55ad7c5b211d42b3
tina.lei <lai85321@gmail.com>
2022-06-18 10:43:33 +0800

    [fix] change component file name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Menu from "./components/menu/menu";
-
+import Nav from "./components/nav/nav";
 function App() {
   return (
     <div style={{ backgroundColor: "#fef4de" }}>
       <Menu />
+      <Nav />
     </div>
   );
 }

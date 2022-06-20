59dd1bb02df9247954fdee96eb21fccd939fe402
tina.lei <lai85321@gmail.com>
2022-06-20 11:08:56 +0800

    [feat] concat balance list api

--- src/components/balance/balance.js
+++ src/components/balance/balance.js
@@ -1,77 +1,43 @@
 import "./balance.css";
+import { useEffect, useState } from "react";
+
+const { REACT_APP_HOST, REACT_APP_API_VERSION } = process.env;
 
 const Balance = () => {
-  const balanceData = [
-    {
-      name: "User 1",
-      balance: [
-        {
-          name: "User 2",
-          amount: 100,
-        },
-        {
-          name: "User 3",
-          amount: 100,
-        },
-      ],
-    },
-    {
-      name: "User 2",
-      balance: [
-        {
-          name: "User 1",
-          amount: -100,
-        },
-      ],
-    },
-    {
-      name: "User 3",
-      balance: [
-        {
-          name: "User 1",
-          amount: -100,
-        },
-      ],
-    },
-  ];
+  const bookId = 1;
+  const [data, setData] = useState([]);
+  const fetchBalanceList = (bookId) => {
+    fetch(
+      `${REACT_APP_HOST}/api/${REACT_APP_API_VERSION}/balance?&bookId=${bookId}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(20);
+        console.log(response);
+        setData(response.data);
+      });
+  };
+  useEffect(() => {
+    fetchBalanceList(bookId);
+  }, []);
   return (
     <div className="bal-container">
-      {/* <div className='bal'>
-                <div className='bal-user'>
-                    <div className='bal-user-image'>
-                    </div>
-                    <div className='bal-user-name'>
-                        name
-                    </div>
-                </div>
-                <div className='bal-details'>
-                    <div className='bal-detail'>
-                    </div>
-                </div>
-            </div> */}
-      {balanceData.map((balance, index) => {
+      {data.map((userBalance, index) => {
         return (
           <div key={index} className="bal">
             <div className="bal-user">
               <div className="bal-user-image"></div>
-              <div className="bal-user-name">{balance.name}</div>
+              <div className="bal-user-name">{userBalance.name}</div>
             </div>
             <div className="bal-details">
-              {balance.balance.map((item, index) => {
-                const owe = item.amount > 0;
-                if (owe) {
-                  return (
-                    <div key={index} className="bal-detail">
-                      Owe {item.name} {Math.abs(+item.amount)}
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div className="bal-detail">
-                      Lend {item.name} {Math.abs(+item.amount)}
-                    </div>
-                  );
-                }
+              {userBalance.balance.details.map((item, index) => {
+                return (
+                  <div key={index} className="bal-detail">
+                    {item}
+                  </div>
+                );
               })}
             </div>
           </div>

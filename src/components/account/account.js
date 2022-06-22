771c346c7ea4e258ec6f08c808cb7090fbe3d37c
tina.lei <lai85321@gmail.com>
2022-06-22 22:39:55 +0800

    [refactor] move function from account component to layout file

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -5,55 +5,8 @@ import { useEffect, useState } from "react";
 import SingleDailyChart from "../chart/chart";
 const { REACT_APP_HOST, REACT_APP_API_VERSION } = process.env;
 
-const Account = () => {
-  const userId = 1;
-  const bookId = 1;
-  const today = new Date();
-  const year = today.getFullYear().toString();
-  const month = (today.getMonth() + 1).toString();
-  const startTime = year.concat("-", month);
-  const [data, setData] = useState([]);
-  const [daily, setDailyData] = useState([]);
-  const [chartData, setChartData] = useState([]);
-  const fetchAccountList = (userId, bookId, startTime) => {
-    fetch(
-      `${REACT_APP_HOST}/api/${REACT_APP_API_VERSION}/accounts?userId=${userId}&bookId=${bookId}&startTime=${startTime}`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        //setData(response.data);
-        setData(response.data);
-        setDailyData(response.data.daily);
-        // setData((data) => ({
-        //   ...response.data,
-        // }));
-      });
-  };
-  useEffect(() => {
-    fetchAccountList(userId, bookId, startTime);
-  }, [startTime]);
-
-  const fetchChartData = (bookId) => {
-    fetch(
-      `${REACT_APP_HOST}/api/${REACT_APP_API_VERSION}/dashboard?bookId=${bookId}`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        setChartData(response.data);
-      });
-  };
-  useEffect(() => {
-    fetchChartData(bookId);
-  }, []);
-
-  const dates = chartData.map((item, index) => item.date);
-
-  const totals = chartData.map((item, index) => item.total);
-
+const Account = (props) => {
+  const { data, daily, dates, totals } = props;
   return (
     <div className="account">
       <div className="left">
--- src/layouts/accountPage.js
+++ src/layouts/accountPage.js
@@ -1,13 +1,64 @@
 import Menu from "../components/menu/menu";
 import Nav from "../components/nav/nav";
 import Account from "../components/account/account";
+import { useEffect, useState } from "react";
+
+const { REACT_APP_HOST, REACT_APP_API_VERSION } = process.env;
 
 function AccountPage() {
+  const userId = 1;
+  const bookId = 1;
+  const today = new Date();
+  const year = today.getFullYear().toString();
+  const month = (today.getMonth() + 1).toString();
+  const startTime = year.concat("-", month);
+  const [data, setData] = useState([]);
+  const [daily, setDailyData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const fetchAccountList = (userId, bookId, startTime) => {
+    fetch(
+      `${REACT_APP_HOST}/api/${REACT_APP_API_VERSION}/accounts?userId=${userId}&bookId=${bookId}&startTime=${startTime}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        //setData(response.data);
+        setData(response.data);
+        setDailyData(response.data.daily);
+        // setData((data) => ({
+        //   ...response.data,
+        // }));
+      });
+  };
+  useEffect(() => {
+    fetchAccountList(userId, bookId, startTime);
+  }, [startTime]);
+
+  const fetchChartData = (bookId) => {
+    fetch(
+      `${REACT_APP_HOST}/api/${REACT_APP_API_VERSION}/dashboard?bookId=${bookId}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setChartData(response.data);
+      });
+  };
+  useEffect(() => {
+    fetchChartData(bookId);
+  }, []);
+
+  const dates = chartData.map((item, index) => item.date);
+
+  const totals = chartData.map((item, index) => item.total);
+
   return (
     <div>
       <Menu />
       <Nav />
-      <Account />
+      <Account daily={daily} data={data} dates={dates} totals={totals} />
     </div>
   );
 }

f904105866f68bdb156cffe864671ff2c9722628
tina.lei <lai85321@gmail.com>
2022-07-05 00:43:32 +0800

    [feat] update setting page

--- src/components/setting/setting.js
+++ src/components/setting/setting.js
@@ -1,18 +1,26 @@
 import "./setting.css";
 const Setting = () => {
   return (
-    <div className="setting-container">
-      <div className="setting-list">
-        <div className="setting-list-header">
-          <div style={{ fontSize: "22px" }}>Setting</div>
+    <div>
+      <div className="setting-nav-container">
+        <div className="setting-nav">
+          <div className="setting-page-logo"></div>
+          <div></div>
         </div>
-        <hr></hr>
-        <>
-          <div className="setting-list-details">
-            <div className="setting-list-detail">name</div>
-            <button className="setting-list-settle">change</button>
+      </div>
+      <div className="setting-container">
+        <div className="setting-list">
+          <div className="setting-list-header">
+            <div style={{ fontSize: "22px" }}>Setting</div>
           </div>
-        </>
+          <hr></hr>
+          <>
+            <div className="setting-list-details">
+              <div className="setting-list-detail">name</div>
+              <button className="setting-list-settle">change</button>
+            </div>
+          </>
+        </div>
       </div>
     </div>
   );
--- src/layouts/settingPage.js
+++ src/layouts/settingPage.js
@@ -1,11 +1,9 @@
 import Menu from "../components/menu/menu";
-import Nav from "../components/nav/nav";
 import Setting from "../components/setting/setting";
 function SettingPage() {
   return (
     <div>
       <Menu />
-      <Nav />
       <Setting />
     </div>
   );

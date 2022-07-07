dd1fbb1d922ef61bd6c85429323d887c01e8c160
tina.lei <lai85321@gmail.com>
2022-07-07 18:14:11 +0800

    [feat] add landing page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,21 +3,63 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="home-nav-container">
-      <div className="home-nav">
-        <div>
-          <div className="home-logo"></div>
-        </div>
-        <div className="home-signin-signup-btns">
-          <Link to="/signIn">
-            <button className="home-signin-btn">Sign in</button>
-          </Link>
-          <Link to="/signUp">
-            <button className="home-signup-btn">Sign up</button>
-          </Link>
+    <>
+      <div className="home-nav-container">
+        <div className="home-nav">
+          <div>
+            <div className="home-logo"></div>
+          </div>
+          <div className="home-signin-signup-btns">
+            <Link to="/signIn">
+              <button className="home-signin-btn">Sign in</button>
+            </Link>
+            <Link to="/signUp">
+              <button className="home-signup-btn">Sign up</button>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="home-main-container">
+        <div className="home-main">
+          <div className="home-main-left">
+            <div className="home-main-slogan">Tracks every cash flows</div>
+            <div className="home-main-content">
+              Record and split payment at the same time
+            </div>
+            <Link to="/signUp">
+              <div className="home-main-start-btn">Try it Now</div>
+            </Link>
+          </div>
+          <div className="home-main-right">
+            <div className="home-main-image"></div>
+          </div>
+        </div>
+      </div>
+      <div className="home-section-container">
+        <div className="home-section">
+          <div className="home-section-texts">
+            <div className="home-section-header">Easy to record payment</div>
+            <div className="home-section-content">
+              Record and split payment at the same time
+            </div>
+          </div>
+          <div className="home-section-images"></div>
+        </div>
+      </div>
+      <div className="home-section-container">
+        <div className="home-section">
+          <div className="home-section-texts">
+            <div className="home-section-header">One click to get balanced</div>
+            <div className="home-section-content">
+              Calculate the easiest way to settle up for you
+            </div>
+          </div>
+          <div className="home-section-images"></div>
+        </div>
+      </div>
+      <div style={{ backgroundColor: "white", height: "60px" }}></div>
+      <div className="home-footer-container"></div>
+    </>
   );
 }
 
